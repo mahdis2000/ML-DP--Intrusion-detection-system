af00ec5c3082d650816bb2f749f068235df02dae
Mahdi-Sohrabi <157570531+mahdis2000@users.noreply.github.com>
2026-06-19 08:34:00 +0330

    Feature - Selection.py

---
+++ src/Feature - Selection.py
@@ -0,0 +1,13 @@
+feature_cols = [
+    'Destination Port',
+    'Flow Duration',
+    'Total Fwd Packets',
+    'Total Backward Packets',
+    'Total Length of Fwd Packets',
+    'Total Length of Bwd Packets',
+    'Fwd Packet Length Max',
+    'Fwd Packet Length Min',
+    'Fwd Packet Length Mean',
+    'Fwd Packet Length Std'
+]
+print("Features:", len(feature_cols))
